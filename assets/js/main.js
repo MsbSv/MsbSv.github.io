23dbfb506a5e16d505cfd9225dd9bc68de86f461
MsbSv <kim020525@naver.com>
2022-07-04 15:58:57 +0900

    update v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Stellar by HTML5 UP
+	Fractal by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -7,8 +7,7 @@
 (function($) {
 
 	var	$window = $(window),
-		$body = $('body'),
-		$main = $('#main');
+		$body = $('body');
 
 	// Breakpoints.
 		breakpoints({
@@ -26,99 +25,26 @@
 				$body.removeClass('is-preload');
 			}, 100);
 		});
-	
-   		$('#container').append('<style>#container, .acceptContainer:before, .logoContainer:before {background: url("https://cdn.discordapp.com/attachments/949597640948060270/951829779491999795/118Z_2012.w015.n001.319B.p15.319.jpg") center fixed }');
-	// Nav.
-		var $nav = $('#nav');
 
-		if ($nav.length > 0) {
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+		else {
 
-			// Shrink effect.
-				$main
-					.scrollex({
-						mode: 'top',
-						enter: function() {
-							$nav.addClass('alt');
-						},
-						leave: function() {
-							$nav.removeClass('alt');
-						},
-					});
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
+			});
 
-			// Links.
-				var $nav_a = $nav.find('a');
-
-				$nav_a
-					.scrolly({
-						speed: 1000,
-						offset: function() { return $nav.height(); }
-					})
-					.on('click', function() {
-
-						var $this = $(this);
-
-						// External link? Bail.
-							if ($this.attr('href').charAt(0) != '#')
-								return;
-
-						// Deactivate all links.
-							$nav_a
-								.removeClass('active')
-								.removeClass('active-locked');
-
-						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-							$this
-								.addClass('active')
-								.addClass('active-locked');
-
-					})
-					.each(function() {
-
-						var	$this = $(this),
-							id = $this.attr('href'),
-							$section = $(id);
-
-						// No section for this link? Bail.
-							if ($section.length < 1)
-								return;
-
-						// Scrollex.
-							$section.scrollex({
-								mode: 'middle',
-								initialize: function() {
-
-									// Deactivate section.
-										if (browser.canUse('transition'))
-											$section.addClass('inactive');
-
-								},
-								enter: function() {
-
-									// Activate section.
-										$section.removeClass('inactive');
-
-									// No locked links? Deactivate all links and activate this section's one.
-										if ($nav_a.filter('.active-locked').length == 0) {
-
-											$nav_a.removeClass('active');
-											$this.addClass('active');
-
-										}
-
-									// Otherwise, if this section's link is the one that's locked, unlock it.
-										else if ($this.hasClass('active-locked'))
-											$this.removeClass('active-locked');
-
-								}
-							});
-
-					});
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
 
 		}
 
 	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000
-		});
+		$('.scrolly')
+			.scrolly({
+				speed: 1500
+			});
 
 })(jQuery);
