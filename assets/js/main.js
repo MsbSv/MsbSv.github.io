693a4c2392a4736bd6faff556bf5a2567f2ce988
MsbSv <97876544+MsbSv@users.noreply.github.com>
2024-04-14 19:31:46 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,6 @@
 	Fractal by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-    Fractal by HTML5 UP
-    html5up.net | @ajlkn
-    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
@@ -49,37 +46,5 @@
 			.scrolly({
 				speed: 1500
 			});
-
-})(jQuery);
-    var $window = $(window),
-        $body = $('body');
-
-    // Breakpoints.
-    breakpoints({
-        xlarge: ['1281px', '1680px'],
-        large: ['981px', '1280px'],
-        medium: ['737px', '980px'],
-        small: ['481px', '736px'],
-        xsmall: ['361px', '480px'],
-        xxsmall: [null, '360px']
-    });
-
-    // Check if mobile
-    if (typeof window.orientation !== 'undefined') {
-        $body.addClass('is-mobile');
-    } else {
-        breakpoints.on('>medium', function() {
-            $body.removeClass('is-mobile');
-        });
-
-        breakpoints.on('<=medium', function() {
-            $body.addClass('is-mobile');
-        });
-    }
-
-    // Scrolly.
-    $('.scrolly').scrolly({
-        speed: 1500
-    });
 
 })(jQuery);
